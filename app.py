fcb2cc0713b219d029680ed729eb15e515f03184
Akekowsik123456 <143722102+Akekowsik123456@users.noreply.github.com>
2023-08-31 20:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import streamlit as st
+st.title('Max of Three Numbers')
+
+a = st.number_input('Enter First number')
+st.write('The first number is ', a)
+
+b = st.number_input('Enter Second number')
+st.write('The second number is ', b)
+
+c = st.number_input('Enter Third number')
+st.write('The third number is ', c)
+
 def max(a,b,c):
     if a>b:
         if a>c :
@@ -9,5 +21,5 @@ def max(a,b,c):
             return b
         else:
             return c
+st.subheader('The Maximum of Three is ', max(10,6,4))
 
-print(max(10,6,4))
